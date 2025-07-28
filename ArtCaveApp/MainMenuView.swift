60aa64235b4726eb3487061e5cab4a15627b8e40
Phuong <phuongtran28072008@gmail.com>
2025-07-28 14:42:17 -0700

    Started on main menu

--- ArtCaveApp/MainMenuView.swift
+++ ArtCaveApp/MainMenuView.swift
@@ -9,10 +9,16 @@ import SwiftUI
 
 struct MainMenuView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 40) {
+            Text("What do you want to create?")
+                .font(.title2)
+                .fontWeight(.semibold)
+        }
     }
 }
 
 #Preview {
-    MainMenuView()
+    NavigationView {
+        MainMenuView()
+    }
 }
