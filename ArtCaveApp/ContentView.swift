efc191c8e9635ba2616128c766c111e9faa63481
Phuong <phuongtran28072008@gmail.com>
2025-07-27 21:22:21 -0700

    test 1.0 start page

--- ArtCaveApp/ContentView.swift
+++ ArtCaveApp/ContentView.swift
@@ -7,18 +7,44 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct StartView: View {
+    @State private var path = NavigationPath()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Art Cave")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            Image("BookIcon")
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .clipShape(Circle())
+                .frame(width: 300, height: 300)
+                .shadow(radius: 15)
+            Spacer()
+            Text("Your creative archive!")
+            Button(action: {
+                path.append("mainMenu")
+            }) {
+                Text("START")
+                    .font(.headline)
+                    .padding()
+                    .frame(width: 200)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(15)
+            }
+            }
+            .padding()
+            .navigationDestination(for: String.self) { value in
+                if value == "mainMenu" {
+                    MainMenuView()
+                }
+            }
         }
-        .padding()
     }
-}
 
 #Preview {
-    ContentView()
+    StartView()
 }
---
+++ ArtCaveApp/MainMenuView.swift
@@ -0,0 +1,18 @@
+//
+//  MainMenuView.swift
+//  ArtCaveApp
+//
+//  Created by Scholar on 7/27/25.
+//
+
+import SwiftUI
+
+struct MainMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainMenuView()
+}
