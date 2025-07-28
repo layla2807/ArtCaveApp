304cb03c07f63dd75c6ebf8f1cd706c7d1a307b9
Phuong <phuongtran28072008@gmail.com>
2025-07-27 20:03:50 -0700

    Initial Commit

---
+++ ArtCaveApp/ArtCaveAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtCaveAppApp.swift
+//  ArtCaveApp
+//
+//  Created by Scholar on 7/27/25.
+//
+
+import SwiftUI
+
+@main
+struct ArtCaveAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
