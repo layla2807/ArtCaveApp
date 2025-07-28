fbb366670d629abf76d9173a4ce7babcdcbfdf8c
Phuong <phuongtran28072008@gmail.com>
2025-07-27 21:50:16 -0700

    Fixed button actions

--- ArtCaveApp/ContentView.swift
+++ ArtCaveApp/ContentView.swift
@@ -8,43 +8,35 @@
 import SwiftUI
 
 struct StartView: View {
-    @State private var path = NavigationPath()
-    
     var body: some View {
-        VStack {
-            Text("Art Cave")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-            Image("BookIcon")
-                .renderingMode(.original)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .clipShape(Circle())
-                .frame(width: 300, height: 300)
-                .shadow(radius: 15)
-            Spacer()
-            Text("Your creative archive!")
-            Button(action: {
-                path.append("mainMenu")
-            }) {
-                Text("START")
-                    .font(.headline)
-                    .padding()
-                    .frame(width: 200)
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(15)
-            }
-            }
-            .padding()
-            .navigationDestination(for: String.self) { value in
-                if value == "mainMenu" {
-                    MainMenuView()
+        NavigationView{
+            VStack {
+                Text("Art Cave")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Image("BookIcon")
+                    .renderingMode(.original)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(Circle())
+                    .frame(width: 300, height: 300)
+                    .shadow(radius: 15)
+                Divider()
+                Text("Your creative archive!")
+                NavigationLink(destination: MainMenuView()){
+                    Text("START")
+                        .font(.headline)
+                        .padding()
+                        .frame(width: 200)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(15)
                 }
             }
+            .padding()
         }
     }
-
+}
 #Preview {
     StartView()
 }
