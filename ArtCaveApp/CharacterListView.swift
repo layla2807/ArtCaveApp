3d17b07e13f589617e0843928435f69aebc7a9c9
Aris Vergara <aubreykvergara@gmail.com>
2025-07-28 21:37:19 -0700

    Tried to add button for the character view, still a wip though

--- ArtCaveApp/CharacterListView.swift
+++ ArtCaveApp/CharacterListView.swift
@@ -9,7 +9,16 @@ import SwiftUI
 
 struct CharacterListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button("+") {
+                   // Action to perform when the button is tapped
+                   print("Button was tapped!")
+               }
+        .font(.system(size:30))
+        .padding()
+        .frame(width: 50)
+        .background(Color.blue)
+        .foregroundColor(.white)
+        .cornerRadius(15)
     }
 }
 
