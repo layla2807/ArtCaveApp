74acdf77e0ff32007390e3166ddcece84cea3a77
Aris Vergara <aubreykvergara@gmail.com>
2025-07-28 22:07:44 -0700

    Made Navigation Link instead of button so it is better

--- ArtCaveApp/CharacterListView.swift
+++ ArtCaveApp/CharacterListView.swift
@@ -9,16 +9,20 @@ import SwiftUI
 
 struct CharacterListView: View {
     var body: some View {
-        Button("+") {
-                   // Action to perform when the button is tapped
-                   print("Button was tapped!")
-               }
-        .font(.system(size:30))
-        .padding()
-        .frame(width: 50)
-        .background(Color.blue)
-        .foregroundColor(.white)
-        .cornerRadius(15)
+        HStack{
+            NavigationLink(destination: CharacterWritingTab()){
+                Text("+")
+                    .font(.system(size:30))
+                    .padding()
+                    .frame(width: 50)
+                    .background(Color.pink)
+                    .foregroundColor(.white)
+                    .cornerRadius(15)
+            }
+          
+        }
+        
+            
     }
 }
 
