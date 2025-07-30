82de910951c129ab66a5c92d5fc10b7ebd9262f2
Lina <linamered3@gmail.com>
2025-07-30 14:03:34 -0400

    Created WorldView, made same CharacterWritingTab changes to WorldWritingTab

--- ArtCaveApp/WorldListView.swift
+++ ArtCaveApp/WorldListView.swift
@@ -8,17 +8,27 @@
 import SwiftUI
 
 struct WorldListView: View {
+    
+    @State private var showWorld = true
+    
     var body: some View {
-        HStack {
-            NavigationLink(destination: WorldWritingTab()){
-                Text("+")
-                    .font(.system(size:30))
-                    .padding()
-                    .frame(width: 50)
-                    .background(Color(red: 189/255, green: 173/255, blue: 150/255))
-                    .foregroundColor(.white)
-                    .cornerRadius(15)
+        VStack {
+            HStack {
+                NavigationLink(destination: WorldWritingTab()){
+                    Text("+")
+                        .font(.system(size:30))
+                        .padding()
+                        .frame(width: 50)
+                        .background(Color(red: 189/255, green: 173/255, blue: 150/255))
+                        .foregroundColor(.white)
+                        .cornerRadius(15)
+                }
             }
+            
+            if showWorld {
+                WorldView(worldName: "")
+            }
+
         }
     }
 }
--- ArtCaveApp/WorldWritingTab.swift
+++ ArtCaveApp/WorldWritingTab.swift
@@ -8,8 +8,41 @@
 import SwiftUI
 
 struct WorldWritingTab: View {
+    
+    @State private var worldName = ""
+    @State private var worldClimate = ""
+    @State private var worldCulture = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment:.leading){
+            Spacer()
+                .frame(height: 50)
+            HStack{
+                Text("Name:")
+                TextField("Insert World Name", text: $worldName)
+            }
+            Spacer()
+                .frame(height: 50)
+            HStack{
+                Text("Climate:")
+                TextField("Insert World Climate", text: $worldClimate)
+            }
+           Spacer()
+                .frame(height: 50)
+            HStack{
+                Text("Culture:")
+                //TextField("Insert Character Backstory", text: $backStory)
+            }
+            TextField("Insert World Culture", text: $worldCulture)
+            Spacer()
+                .frame(height: 50)
+            
+           Button("Save"){
+               }
+          
+        }
+        .padding()
+
     }
 }
 
