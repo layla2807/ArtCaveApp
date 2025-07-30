f4c90d4810888e7ebff7011c9f881a736798c6a3
Aris Vergara <aubreykvergara@gmail.com>
2025-07-30 08:14:31 -0700

    tried to add text fields and things to character writing tab

--- ArtCaveApp/CharacterWritingTab.swift
+++ ArtCaveApp/CharacterWritingTab.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct CharacterWritingTab: View {
     @State private var charName = ""
+    @State private var backStory = ""
+    @State private var Age = ""
     var body: some View {
         VStack{
             HStack{
@@ -16,6 +18,18 @@ struct CharacterWritingTab: View {
                 TextField("Insert Character Name", text: $charName)
                 
             }
+            .padding()
+            HStack{
+                Text("Age:")
+                TextField("Insert Character Age", text: $Age)
+                
+            }
+            .padding()
+            HStack{
+                Text("Backstory:")
+                //TextField("Insert Character Backstory", text: $backStory)
+            }
+            TextField("Insert Character Backstory", text: $backStory)
             .padding()
         }
     }
