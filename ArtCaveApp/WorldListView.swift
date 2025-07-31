c76c33c1697fae221034d8a040228cc6dcda31dc
Aris Vergara <aubreykvergara@gmail.com>
2025-07-31 10:49:36 -0700

    Delete feature and look of world tab looks like character (MUST ADD EDIT FEATURE)

--- ArtCaveApp/WorldListView.swift
+++ ArtCaveApp/WorldListView.swift
@@ -14,45 +14,60 @@ struct WorldListView: View {
     @Query var worldList: [World]
     @Environment(\.modelContext) var modelContext
     
-    var body: some View {
-        VStack {
-            
-            NavigationLink(destination: WorldWritingTab()){
-                Text("+")
-                    .font(.system(size:30))
-                    .padding()
-                    .frame(width: 50)
-                    .background(Color(red: 189/255, green: 173/255, blue: 150/255))
-                    .foregroundColor(.white)
-                    .cornerRadius(15)
-            }
-            List{ ForEach(worldList) { item in
-                
-                VStack(alignment: .leading) {
-                    
-                    Text("name: \(item.name)")
-                    
-                    Text("climate: \(item.climate)")
-                    
-                    Text("culture: \(item.culture)")
-                    
-                }
-                
-            }
-            .onDelete(perform: deleteWorld)
-                
-            }
-            
-        }
-    }
-    func deleteWorld(at offsets: IndexSet) {
-        for offset in offsets {
-            let charWorld = worldList[offset]
-            modelContext.delete(charWorld)
-        }
-    }
     
+    let columns = [
+        GridItem(.flexible(), spacing:  16),
+        GridItem(.flexible(), spacing:  16)]
+    
+    var body: some View {
+        
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 16, content: {
+                NavigationLink(destination: WorldWritingTab()){
+                    Text("+")
+                        .font(.system(size: 60))
+                        .frame(maxWidth: .infinity, minHeight: 180)
+                        .background(Color(red: 189/255, green: 173/255, blue: 150/255))
+                        .foregroundColor(.white)
+                        .cornerRadius(15)
+                }
+                ForEach(worldList) { item in
+                    
+                    ZStack(alignment: .topTrailing){
+                        VStack(alignment: .leading) {
+                            
+                            Text("name: \(item.name)")
+                            
+                            Text("climate: \(item.climate)")
+                            
+                            Text("culture: \(item.culture)")
+                            
+                        }
+                        .padding()
+                        .frame(maxWidth: .infinity, minHeight: 180)
+                        .background(Color(red: 189/255, green: 173/255, blue: 150/255))
+                        .cornerRadius(15)
+                    }
+                    .contextMenu {
+                        Button(role: .destructive) {
+                            modelContext.delete(item)
+                        } label: {
+                            Label("Delete", systemImage: "trash")
+                        }
+                    }
+                }
+            
+                    
+                
+                
+                
+            })
+            .padding()
+        }
+    }
 }
+    
+
 
 #Preview {
     WorldListView()
