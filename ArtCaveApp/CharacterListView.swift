bf0e15079f51230bf7b4c264ac4872cc6d88278b
Phuong <phuongtran28072008@gmail.com>
2025-07-31 00:16:18 -0700

    Character view like pinterest, altered delete function

--- ArtCaveApp/CharacterListView.swift
+++ ArtCaveApp/CharacterListView.swift
@@ -13,39 +13,46 @@ struct CharacterListView: View {
     @Query var charList: [Character]
     @Environment(\.modelContext) var modelContext
     
+    let columns = [
+        GridItem(.flexible(), spacing:  16),
+        GridItem(.flexible(), spacing:  16)
+    ]
+    
     var body: some View {
-        VStack {
-                NavigationLink(destination: CharacterWritingTab()){
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 16, content: {
+                
+                NavigationLink(destination: CharacterWritingTab()) {
                     Text("+")
-                        .font(.system(size:30))
-                        .padding()
-                        .frame(width: 50)
+                        .font(.system(size: 60))
+                        .frame(maxWidth: .infinity, minHeight: 180)
                         .background(Color(red: 189/255, green: 173/255, blue: 150/255))
                         .foregroundColor(.white)
                         .cornerRadius(15)
-            }
-            List { ForEach (charList) {item in
-                VStack(alignment: .leading) {
-                    Text("Name: \(item.name)")
-                    Text("age: \(item.age)")
-                    Text("backstory: \(item.backstory)")
-                    
                 }
-            }
-            .onDelete(perform: deleteCharacter)
+                ForEach(charList) { item in
+                    VStack(alignment: .leading) {
+                        Text("Name: \(item.name)")
+                        Text("age: \(item.age)")
+                        Text("backstory: \(item.backstory)")
+                    }
+                    .padding()
+                    .frame(maxWidth: .infinity, minHeight: 180)
+                    .background(Color(red: 189/255, green: 173/255, blue: 150/255))
+                    .cornerRadius(15)
+                    .contextMenu {
+                        Button(role: .destructive) {
+                            modelContext.delete(item)
+                        } label: {
+                            Label("Delete", systemImage: "trash")
+                        }
+                    }
+                }
+            })
+            .padding()
                             }
         }
-        
-            
     }
-    func deleteCharacter(at offsets: IndexSet) {
-        for offset in offsets {
-            let charItem = charList[offset]
-            modelContext.delete(charItem)
-        }
-        
-    }
-}
 
 #Preview {
     CharacterListView()
