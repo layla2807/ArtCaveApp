d6bca04f17f9e69094e61f1ed1c5ccab96adaee8
Aris Vergara <aubreykvergara@gmail.com>
2025-07-30 18:49:58 -0700

    can delete worlds

--- ArtCaveApp/WorldListView.swift
+++ ArtCaveApp/WorldListView.swift
@@ -12,41 +12,46 @@ struct WorldListView: View {
     
     //@State private var showWorld = true
     @Query var worldList: [World]
+    @Environment(\.modelContext) var modelContext
     
     var body: some View {
         VStack {
-           
-                NavigationLink(destination: WorldWritingTab()){
-                    Text("+")
-                        .font(.system(size:30))
-                        .padding()
-                        .frame(width: 50)
-                        .background(Color(red: 189/255, green: 173/255, blue: 150/255))
-                        .foregroundColor(.white)
-                        .cornerRadius(15)
+            
+            NavigationLink(destination: WorldWritingTab()){
+                Text("+")
+                    .font(.system(size:30))
+                    .padding()
+                    .frame(width: 50)
+                    .background(Color(red: 189/255, green: 173/255, blue: 150/255))
+                    .foregroundColor(.white)
+                    .cornerRadius(15)
             }
-            List(worldList) { item in
-
-                                VStack(alignment: .leading) {
-
-                                    Text("name: \(item.name)")
-
-                                    Text("climate: \(item.climate)")
-
-                                    Text("culture: \(item.culture)")
-
-                                }
-
-                            }
-
-
-
-
-
-
+            List{ ForEach(worldList) { item in
+                
+                VStack(alignment: .leading) {
+                    
+                    Text("name: \(item.name)")
+                    
+                    Text("climate: \(item.climate)")
+                    
+                    Text("culture: \(item.culture)")
+                    
+                }
+                
+            }
+            .onDelete(perform: deleteWorld)
+                
+            }
             
         }
     }
+    func deleteWorld(at offsets: IndexSet) {
+        for offset in offsets {
+            let charWorld = worldList[offset]
+            modelContext.delete(charWorld)
+        }
+    }
+    
 }
 
 #Preview {
